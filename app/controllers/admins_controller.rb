2b0f1326432793bf0b1ee556e7e23c7e1be512d8
FuruyaYuuki <yuki.furu.0317@gmail.com>
2021-10-09 13:31:59 +0000

    add admin

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,12 @@
+class AdminsController < ApplicationController
+  
+  protected
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Admin.create!(
+   email: 'yuki.furu.0317@gmail.com',
+   password: 'Yuki0321',
+   name: 'Furuya Yuuki'
+)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
