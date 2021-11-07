cc17e46ae24118901d872b86a3e1708b098f31cf
FuruyaYuuki <yuki.furu.0317@gmail.com>
2021-11-07 13:04:40 +0000

    update admin show, header, top

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -11,4 +11,18 @@ class Admin < ApplicationRecord
   # mount
   mount_uploader :avatar, AvatarUploader
   
+  # update_without_current_password
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+  
 end
