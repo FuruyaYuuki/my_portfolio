b9947bf2595bf48f3665349001502a67202b598e
FuruyaYuuki <yuki.furu.0317@gmail.com>
2021-12-25 12:45:07 +0000

    update layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # Spotify API
+  require 'rspotify'
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+  
   private
 
   def configure_permitted_parameters
--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,13 +1,11 @@
 class SearchsController < ApplicationController
-  require 'rspotify'
-  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
   
   def search
     @model = params["model"]
     @text = params["text"]
     @records = search_for(@model, @text)
     if params[:text].present?
-      @tracks = RSpotify::Track.search(params[:text], offset: 0, market: nil)
+      @tracks = RSpotify::Track.search(params[:text], offset: 0, market: 'JP')
     end
   end
   
