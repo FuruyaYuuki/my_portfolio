f909f484bac89a33cf70882add24c3a650a7af90
FuruyaYuuki <yuki.furu.0317@gmail.com>
2021-12-26 08:18:52 +0000

    add validation, update readme

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,11 @@
 class Answer < ApplicationRecord
+  
+  # association
   belongs_to :question
   belongs_to :admin
+  
+  # validation
+  validates :text, presence: { message: "入力してください" }
+  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています" }
+  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,12 @@ class Question < ApplicationRecord
   # association
   belongs_to :user
   has_many :likes, dependent: :destroy
-  
   has_one :answer, dependent: :destroy
+  
+  # validation
+  validates :title, :text, presence: { message: "入力してください" }
+  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています" }
+  validates_length_of :title, maximum: 20
+  validates_length_of :text, maximum: 100
   
 end
