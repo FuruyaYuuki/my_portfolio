0937fd0f9bb083463e416ea16539b54df840d120
FuruyaYuuki <yuki.furu.0317@gmail.com>
2021-12-30 18:22:48 +0000

    update registration, session

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,9 @@ class Answer < ApplicationRecord
   belongs_to :admin
   
   # validation
+  validates :text, presence: true, if: :admin_signed_in?
   validates :text, presence: { message: "入力してください" }
-  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています" }
+  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています", allow_nil: true }
+  validates_length_of :text, maximum: 200, allow_nil: true
   
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,9 +6,10 @@ class Question < ApplicationRecord
   has_one :answer, dependent: :destroy
   
   # validation
+  validates :text, :title, presence: true, if: :user_signed_in?
   validates :title, :text, presence: { message: "入力してください" }
-  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています" }
-  validates_length_of :title, maximum: 20
-  validates_length_of :text, maximum: 100
+  validates :text, uniqueness: { case_sensitive: false, message: "すでに質問されています", allow_nil: true }
+  validates_length_of :title, maximum: 20, allow_nil: true
+  validates_length_of :text, maximum: 100, allow_nil: true
   
 end
